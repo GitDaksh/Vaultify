d8faa0e0bb10cb35e5f30d4cae29cc722ddf06d5
Daksh <daksh.java.util@gmail.com>
2024-12-16 19:36:17 +0530

    basic setup

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-poppins',
+
+})
+
+export const metadata: Metadata = {
+  title: "Vaultify",
+  description: "Secure and user-friendly cloud storage app for managing, organizing, and accessing your files anytime, anywhere",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
